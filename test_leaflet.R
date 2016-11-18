2d3f4dea04930441c4ba484a81954399d12c3569
Mkys <munky39@hotmail.com>
2016-11-18 11:06:14 +0000

    Ajout d'un marker sur hotel de ville pour carte leaflet

--- test_leaflet.R
+++ test_leaflet.R
@@ -2,5 +2,6 @@ library(leaflet)
 
 m <- leaflet() %>%
   addTiles() %>%
-  addMarkers(lng=4.831719, lat=45.757812, popup= "Place Bellecour")
+  addMarkers(lng = 4.831719, lat = 45.757812, popup= "Place Bellecour")%>%
+  addMarkers(lng = 4.835302, lat = 45.767662, popup = "Hôtel de ville")
 m
